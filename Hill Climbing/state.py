53c4b0139d0319dea100a7a9ba7ee2680a2142c2
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2019-12-29 00:14:45 +0000

    Added neighbor generation function

--- Hill Climbing/state.py
+++ Hill Climbing/state.py
@@ -8,3 +8,15 @@ class State:
     def state_evaluation(self):
         tupl = matrix[self.x][self.y]
         return (tupl[0] + tupl[1])/2
+
+    def generate_neighbors(self):
+        list_to_be_ret = []
+        if self.x - 1 > 0:
+            list_to_be_ret.append(State(self.matrix, self.x - 1, self.y)) # not sure if I need to copy objects before passing as argument
+        if self.x + 1 < len(self.matrix):
+            list_to_be_ret.append(State(self.matrix, self.x + 1, self.y)) 
+        if self.y - 1 > 0:
+            list_to_be_ret.append(State(self.matrix, self.x, self.y - 1)) 
+        if self.y + 1 < len(self.matrix):
+            list_to_be_ret.append(State(self.matrix, self.x, self.y + 1)) 
+        return list_to_be_ret
