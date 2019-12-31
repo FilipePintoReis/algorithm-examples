c4a72d931928cd44e1c615b8b8188fa88bb45600
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2019-12-31 03:09:39 +0000

    chooseNextNode logic is complete, untested

--- Ant Colony/Ant.py
+++ Ant Colony/Ant.py
@@ -21,13 +21,42 @@ class Ant:
         def setCurrentNode(self, node):
                 self.currentNode = node
 
+        def setObjectiveHome(self):
+                self.currentObjective = self.sourceName
+
+        def addEdgeToPath(self, edge):
+                if self.currentObjective == self.objective:
+                        self.pathToObjective.append(edge)
+                else: self.pathBackHome.append(edge)
+
         def chooseNextNode(self):
                 randN = random.random()
                 accumulator = 0
-                for edge in self.graph.getNodeEdges(self.currentNode):
-                     edgePheromones = edge.pheromoneLevel
-                     edgeQuality = edge.edgeQuality()
-                     accumulator += (edgePheromones ** self.alpha) + (edgeQuality ** self.beta)
-        
+                nodeEdges = self.graph.getNodeEdges(self.currentNode)
 
-        #def setPheromones
+                for edge in nodeEdges:
+                        edgePheromones = edge.pheromoneLevel
+                        edgeQuality = edge.edgeQuality()
+                        accumulator += (edgePheromones ** self.alpha) + (edgeQuality ** self.beta)
+
+                randomN = random.uniform(0, accumulator)
+
+                ac1 = 0
+                ac2 = 0
+                for edge in range(0, len(nodeEdges)):
+                        ac1 = ac2
+                        edgePheromones = edge.pheromoneLevel
+                        edgeQuality = edge.edgeQuality()
+                        ac2 += (edgePheromones ** self.alpha) + (edgeQuality ** self.beta)
+                        if randomN > ac1 and randomN < ac2: 
+                                return self.graph.getNode(edge.node2)
+
+        def isObjectiveNode(self):
+                if self.currentNode.name == self.objective: return True
+                else: return False
+
+        def isSourceNode(self):
+                if self.currentNode.name == self.sourceName: return True
+                else: return False
+
+        #def setPheromones(self):
