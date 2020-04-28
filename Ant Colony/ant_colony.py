bc62ae2cdbb3ebc00740edfd55cac7cb1e52a919
Filipe Pinto Reis <filipepintodosreis@gmail.com>
2020-04-28 17:35:15 +0100

    Ant colony example

---
+++ Ant Colony/ant_colony.py
@@ -0,0 +1,70 @@
+from graph import Graph
+from node  import Node
+from edge import Edge
+from ant import Ant
+
+
+# Create graph
+# g = Graph()
+
+# # Add initial node
+# g.add_root(Node(1,g.get_id()))
+
+# current_ant = Ant(1,1,g.root,g)
+# ret_node = current_ant.expand_node()
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # current_ant.update_node(ret_node)
+# # # # # # # # # # # # # # # # # # # # # # # # # # current_ant.expand_node()
+# # # # # # # # # # # # # # # # # # # # # # # # # # print(str(g))
+# Gerar ants
+
+
+def calculate_if_rose(new, old):
+    if new/old > 1.1:
+        return True
+    return False
+
+def ant_colony():
+    iterations_total = 50
+    iterator = iterations_total
+    percentage = 0.1
+    best_10 = []
+
+    g = Graph()
+    g.add_root(Node(1,g.get_id()))
+
+    maximum = g.root
+    best_10.append(g.root)
+
+    while iterator > 0:
+        current_ant = Ant(1,1,g.root,g)
+
+        while not current_ant.explore():
+            if len(current_ant.get_curr_node_new_neighbours()) == 0:
+                break
+            else:
+                current_ant.chooses_path()
+            
+        current_ant.deposit_pheromones()
+        ret_node = current_ant.expand_node()
+
+        if len(best_10) < 10:
+            best_10.append(ret_node)
+        else:
+            
+
+        if maximum.heuristic < ret_node.heuristic:
+            maximum = ret_node
+
+        if calculate_if_rose(ret_node.heuristic, maximum.heuristic):
+            iterator = iterations_total
+        else:
+            iterator -= 1
+
+    return g
+
+    
+
+g = ant_colony()
+print(g)
---
+++ Ant Colony/node.py
@@ -0,0 +1,50 @@
+from random import randint
+
+class Node:
+    def __init__(self, state, id):
+        self.state = state
+        self.edges = {}
+        self.id = id
+        self.heuristic = self.get_heuristic()
+    
+    def add_edge(self, edge):
+        if str(edge) not in self.edges:
+            self.edges[str(edge)] = edge
+
+    def get_nodes(self):
+        l = []
+        for edge in self.edges.values():
+            l.append(edge.get_other(self))
+        return l
+        
+    def get_heuristic(self): # To be replaced by state.get_score
+        return self.state
+
+    def get_sum(self, alpha, beta):
+        s = 0
+        for edge in self.edges.values():
+            other_node = None
+            if edge.node_1.id != self.id:
+                other_node = edge.node_1
+            else: other_node = edge.node_2
+
+            s += (edge.pheromones ** alpha) * (other_node.heuristic ** beta)
+        return s
+
+    def return_new_state(self): # To be replaced by random_neighbour
+        return randint(0, 100)
+
+    def __str__(self):
+        return 'Node: ' + str(self.state)
+
+    def __eq__(self, other):
+        if str(self) == str(other):
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.heuristic < other.heuristic:
+            return self
+        else:
+            return other
